c43d67e298aabc196c989fb2cec1fdd20c44083f
Hongyu Lin <131169223+Erikline@users.noreply.github.com>
2025-12-11 00:14:53 +0800

    fix

--- Turing's Artistic Realm-FrontEnd/server.js
+++ Turing's Artistic Realm-FrontEnd/server.js
@@ -4,24 +4,22 @@ const path = require("path");
 const app = express();
 
 // 提供 public 文件夹中的静态文件
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
-// 提供 Three.js 的构建文件
+// Three.js 静态资源
 app.use("/build/", express.static(path.join(__dirname, "node_modules/three/build")));
-
-// 提供 Three.js 的示例模块
 app.use("/jsm/", express.static(path.join(__dirname, "node_modules/three/examples/jsm")));
 
-// 提供 GSAP 的静态文件
+// GSAP 静态资源
 app.use("/gsap/", express.static(path.join(__dirname, "node_modules/gsap")));
 
-// 重定向根路径到 Home.html
-app.get('/', (req, res) => {
-    res.redirect('/Home.html');
+// 根路径重定向
+app.get("/", (req, res) => {
+    res.redirect("/Home.html");
 });
 
 // 启动服务器
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
+    console.log(`server is running on port ${PORT}`);
 });
